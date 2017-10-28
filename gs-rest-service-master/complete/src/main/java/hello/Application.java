3f4714d0d7c494d80fcfc87ea30ea0db1d133ea4
Maximilian Fuchs <maximilian.fuchs@stud.tu-darmstadt.de>
2017-10-28 14:16:40 +0200

    Spriung Application Running

--- gs-rest-service-master/complete/src/main/java/hello/Application.java
+++ gs-rest-service-master/complete/src/main/java/hello/Application.java
@@ -1,12 +1,13 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    	
     }
 }
