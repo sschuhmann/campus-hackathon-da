8e78bb15aecf26b21698f9d9536be478f4095356
Steffen Schuhmann <mail@sschuhmann.de>
2017-10-28 18:26:27 +0200

    more

--- Backend/server.py
+++ Backend/server.py
@@ -2,30 +2,21 @@ from flask import Flask
 import logging
 from flask import request
 
-from routing import route
-
 server = Flask(__name__)
-
-route = routing.Route()
 
 @server.route('/route/')
 def get_route():
     logging.info("Request")
-    start = request.args.get('start')
-    destination = requests.args.get('destination')
-    time = requests.args.get('date_time')
+    start = request.json
     logging.info(str(content))
-    route.get_route_car(content[start],
-                content[destination])
 
     return "Hallo Welt"
 
-@server.route('')
+@server.route('/')
 def store_user_settings():
     logging.info("Store settings")
     content = request.json
     logging.info(str(content))
-
     try:
         pass
     except:
@@ -33,4 +24,3 @@ def store_user_settings():
 
 if __name__ == '__main__':
     server.run(debug=True)
-s
