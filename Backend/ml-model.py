84fccb8f418f38a97ad3de3418011122e5250b33
Maximilian Fuchs <maximilian.fuchs@stud.tu-darmstadt.de>
2017-10-28 23:14:55 +0200

    ML WIP

--- Backend/ml-model.py
+++ Backend/ml-model.py
@@ -1,17 +1,36 @@
 import time
 import json
 import datetime
+from sklearn.neural_network import MLPClassifier
 from twython import Twython
+import pickle
 
 def feedback(vehicle, location):
     # receive the actual transport vehicle that was taken
 
     # dataframe creation
     create_dataframe(location, vehicle)
-    # create a date entry and add it to the data table
+    #add dataframe to the data table
+    writeFrame(dataframe)
 
+    # load ml classifier
+    # if classifier not existent
+        #create classifier
+        classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    #else
+        #load classifier
+        f = open('model/classifier.pickle', 'rb')
+        classifier = pickle.load(f)
+        f.close()
 
-    # update the ml-model
+    X = [value for dataframe[]removekey(dataframe, 'vehicle')]
+    # update the ml-classifier
+    classifier.partial_fit(X,y)
+
+    #store the classifier in /model
+    f = open('model/classifier.pickle', 'wb')
+    pickle.dump(classifier, f)
+    f.close()
     return ''
 
 def models_opinion(location):
@@ -35,8 +54,22 @@ def create_dataframe(location, vehicle):
     dataframe['DBVerspätungsScore'] = recent_schlagwort_score('Bahn Verspätung', location)
     dataframe['FerienScore'] = recent_schlagwort_score('Ferien', location)
     dataframe['Feiertag'] = recent_schlagwort_score('Feiertag', location)
+    dataframe['Sonnig'] = recent_schlagwort_score('Sonnig', location)
+    dataframe['Regen'] = recent_schlagwort_score('Regen', location)
+    dataframe['Schnee'] = recent_schlagwort_score('Schnee', location)
+    dataframe['Sturm'] = recent_schlagwort_score('Feiertag', location)
+    dataframe['Verkehr'] = recent_schlagwort_score('Verkehr', location)
+    dataframe['Fahrrad'] = recent_schlagwort_score('Fahrrad', location)
+    dataframe['Auto'] = recent_schlagwort_score('Auto', location)
+    dataframe['Bahn'] = recent_schlagwort_score('Bahn', location)
 
     #set vehicle
+    if vehicle == 'Car':
+        vehicle = 0
+    if vehicle == 'Bike':
+        vehicle == 1
+    if vehicle == 'Train':
+        vehicle = 3
     dataframe['vehicle'] = vehicle
 
     return dataframe
@@ -62,4 +95,21 @@ def recent_schlagwort_score(schlagwort, location):
             if location in status['text']:
                 count += 2
 
-    return count/100
+    return count/300
+
+def writeFrame(dataframe):
+    string_dataframe = ''
+    for key in dataframe.keys():
+        string_dataframe.append(key + ':' + dataframe[key] + ',')
+
+    f = open('datatable', 'a')
+    f.write(string_dataframe + '\n')  # python will convert \n to os.linesep
+    f.close()
+
+def read_dataset():
+    return ''
+
+def removekey(d, key):
+    r = dict(d)
+    del r[key]
+    return r
