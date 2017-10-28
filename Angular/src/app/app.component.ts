4eb13d51b6fc669f1527aa10af1b7bd72526aabf
Florian <florian@westmeier.eu>
2017-10-28 21:12:32 +0200

    Post hinzugefügt.

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +14,21 @@ export class AppComponent {
   start: string;
   destination: string;
   date_time: string;
+  results: string;
+
+
+  constructor(private httpClient: HttpClient) {
+
+  }
 
   getValues() {
-    console.log(this.start);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+    headers.append('Access-Control-Allow-Origin', '*');
+
+    this.httpClient.post('http://192.168.179.43:12000/route', {start: this.start, destination: this.destination, date_time: this.date_time},
+    {headers: headers})
+    .subscribe(data => {
+      this.results = data['results'];
+    });
   }
 }
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
